f0416735af7ed164b1cadeb731f94ba3bad19c7a
TomiwaOgun <ogunleyetomiwa0@gmail.com>
2025-03-16 20:17:22 -0600

    changes to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '', // Set this if your app is not hosted at the root domain
+    assetPrefix: '', // Set this if your assets are not hosted at the root domain
+};
 
 export default nextConfig;
